81a603cc5cee257183e779d90438bde017e12ca0
Ishitw <47420924+IshitwaAgrawal@users.noreply.github.com>
2020-06-14 21:32:03 +0530

    first

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const header = () =>{
+    return(
+        <div className="jumbotron">
+            My To-Do-List
+        </div>
+    );
+}
+
+export default header;
