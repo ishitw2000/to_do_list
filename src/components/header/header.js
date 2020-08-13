1f541d929b2348682ef91b52cb4c003106ca752b
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-08-14 00:43:14 +0530

    new

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,13 +3,6 @@ import './header.css';
 
 const header = (props) =>{
     return(
-        // <div className="text-light bg-success jumbotron">
-        //     <div className="container" style={{"height":"20px","alignItems":"middle"}}>
-        //         <h3 class="head-title">
-        //             <u><b>{props.title}</b></u>
-        //         </h3>
-        //     </div>
-        // </div>
         <div className="jumbotron jumbotron-fluid bg-success text-light">
             <div className="container">
                 <h1 className="display-5 lead">{props.title}</h1>
